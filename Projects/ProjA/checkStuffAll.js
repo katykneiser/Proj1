fd71cb8901accf61e5aaa89848823faf6084b0bf
Katy Kneiser <kneiserk@gmail.com>
2012-04-28 17:13:20 -0500

    single quote attempt

--- Projects/ProjA/checkStuffAll.js
+++ Projects/ProjA/checkStuffAll.js
@@ -1,6 +1,6 @@
 function checkCheckBoxes() {
-	var M = document.getElementById("edit-md-options-06-2012-wrapper").setAttribute("checked","checked");
-	var N = document.getElementById("edit-md-options-07-2012-wrapper").setAttribute("checked","checked");
+	var M = document.getElementById("edit-md-options-06-2012-wrapper").setAttribute('checked','checked');
+	/* var N = document.getElementById("edit-md-options-07-2012-wrapper").setAttribute("checked","checked");
 	var Q = document.getElementById("edit-md-options-08-2012-wrapper").setAttribute("checked","checked");
 	var U = document.getElementById("edit-md-options-09-2012-wrapper").setAttribute("checked","checked");	
 	var Z = document.getElementById("edit-md-options-12-2012-wrapper").setAttribute("checked","checked");
@@ -10,6 +10,7 @@ function checkCheckBoxes() {
 	var rU = document.getElementById("edit-md-options-09-2013-wrapper").setAttribute("checked","checked");
 	var rZ = document.getElementById("edit-md-options-12-2013-wrapper").setAttribute("checked","checked");
 	var rH = document.getElementById("edit-md-options-03-2014-wrapper").setAttribute("checked","checked");
+*/
 
 	alert("DONE");
 }
